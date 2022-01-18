afa08d08f5cd508526910c5131a38df3c0c3f56e
Ravi Ranjan <raviranjan3570@gmail.com>
2022-01-18 22:57:44 +0530

    added parsing logic for author dump

--- src/main/java/com/betterreads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/betterreads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,6 +1,10 @@
 package com.betterreads.betterreadsdataloader;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
@@ -8,7 +12,10 @@ import com.betterreads.betterreadsdataloader.author.Author;
 import com.betterreads.betterreadsdataloader.author.AuthorRepository;
 import com.betterreads.betterreadsdataloader.connection.DataStaxAstraProperties;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -22,23 +29,52 @@ public class BetterReadsDataLoaderApplication {
 	@Autowired
 	private AuthorRepository repository;
 
+	@Value("${datadump.location.authors}")
+	private String authorsDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
 	}
 
+	private void initAuthors() {
+		Path path = Paths.get(authorsDumpLocation);
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				String jsonString = line.substring(line.indexOf("{"));
+				JSONObject jsonObject;
+				try {
+					jsonObject = new JSONObject(jsonString);
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+					repository.save(author);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initWorks() {
+
+	}
+
 	@PostConstruct
-	public void start(){
-		Author author = new Author();
-		author.setId("1");
-		author.setName("Ravi");
-		author.setPersonalName("Golu");
-		repository.save(author);
+	public void start() {
+		initAuthors();
+		initWorks();
 	}
 
 	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(
-		DataStaxAstraProperties properties){
-			Path bundle = properties.getSecureConnectBundle().toPath();
-			return builder -> builder.withCloudSecureConnectBundle(bundle);
+			DataStaxAstraProperties properties) {
+		Path bundle = properties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 }
